aebcc642659884520f5fd4b08c38b2de87e901c4
周鲁东 <ludongzhou@gmail.com>
2016-01-13 23:57:25 +0800

    add kernel_main.c but error: ld: unknown option: -Tlink.ld

---
+++ src/kernel_main.c
@@ -0,0 +1,12 @@
+//
+// Created by 周鲁东 on 16/1/13.
+//
+
+// main.c -- Defines the C-code kernel entry point, calls initialisation routines.
+// Made for JamesM's tutorials
+
+int kernel_main(struct multiboot *mboot_ptr)
+{
+    // All our initialisation calls will go in here.
+    return 0xDEADBABA;
+}
